d64ba3b671e18483b0be928380db1348edbbd90e
Kristijan Vidaković <kiki11223333@gmail.com>
2022-11-21 22:21:39 +0100

    Create muzej.sql

---
+++ SQL_Exercises/muzej.sql
@@ -0,0 +1,38 @@
+# c:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < E:\Java\Java_Learning\SQL_Exercises\muzej.sql
+
+drop database if exists muzej;
+create database muzej;
+use muzej;
+
+create table izlozba(
+    sifra int not null primary key auto_increment,
+    naziv varchar(80),
+    sponzor int,
+    kustos int    
+);
+
+create table kustos(
+    sifra int not null primary key auto_increment,
+    ime varchar(20),
+    prezime varchar(20),
+    oib char(11),
+    iban varchar(50)          
+);
+
+create table djelo(
+    sifra int not null primary key auto_increment,
+    autor varchar(40),
+    vrijednost decimal(18,2),
+    tehnika varchar(40),
+    izlozba int    
+);
+
+create table sponzor(
+    sifra int not null primary key auto_increment,
+    naziv varchar(80),
+    iznos decimal(18,2)         
+);
+
+alter table izlozba add foreign key (kustos) references kustos(sifra);
+alter table izlozba add foreign key (sponzor) references sponzor(sifra);
+alter table djelo add foreign key (izlozba) references izlozba(sifra);
