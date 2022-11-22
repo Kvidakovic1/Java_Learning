445978ddb00b28ce2430a7882035fdc60bf925de
Kristijan Vidaković <kiki11223333@gmail.com>
2022-11-23 00:08:36 +0100

    Uneseni podaci u tablice

--- SQL_Exercises/kud.sql
+++ SQL_Exercises/kud.sql
@@ -34,3 +34,23 @@ create table clan_nastup(
 alter table nastup add foreign key (mjesto) references mjesto(sifra);
 alter table clan_nastup add foreign key (clan) references clan(sifra);
 alter table clan_nastup add foreign key (nastup) references nastup(sifra);
+
+insert into mjesto(sifra,naziv,zupanija,postanski_broj)
+values(null,'Tiborjanci','Osjecko Baranjska', '31554'),
+(null,'Osijek','Osjecko Baranjska','31551'),
+(null,'Virovitica', 'Viroviticko Podravska', '34213');
+
+insert into clan(sifra, ime, prezime, oib)
+values(null,'Ankica','Pervan', '74259823585'),
+(null,'Branka','Kukec','31339135593'),
+(null,'Ivo','Podboj','38322842246');
+
+
+insert into nastup(sifra,mjesto,vrijeme,max_gledatelja)
+values(null,1,'2022-12-23 20:00:00', 500),
+(null, 2, '2022-12-31 19:00:00', 1500),
+(null, 3,  '2023-01-10 16:00:00', 1000);
+
+
+insert into clan_nastup(clan,nastup)
+values(1,1),(2,1),(3,1);
