909a7b87d398b83bbd1cb0daa8c6ff12f684ae49
Kristijan Vidaković <kiki11223333@gmail.com>
2022-11-25 18:39:50 +0100

    Uneseni podaci

--- SQL_Exercises/taxi_sluzba.sql
+++ SQL_Exercises/taxi_sluzba.sql
@@ -35,3 +35,22 @@ create table voznja(
 alter table vozilo add foreign key (vozac) references vozac(sifra);
 alter table voznja add foreign key (putnik) references putnik(sifra);
 alter table voznja add foreign key (vozilo) references vozilo(sifra);
+
+
+insert into putnik(sifra, ime,prezime,oib,broj_telefona)
+values(null,'Luka','Habus','30529884741', '+1 208-969-9037'),
+(null,'Marko','Huljak','30772240538','+376 539 569'),
+(null,'Filip','Begic','80481548717','+376 345 880');
+
+
+insert into vozac(sifra,ime,prezime,oib,broj_telefona)
+values(null,'Dominik','Perokovic','10711478243','+54 9 3327 45-1377'),
+(null,'Marin','Mijakic','26360930301','+54 9 2966 31-7638');
+
+
+insert into vozilo(sifra, marka,model, godina_proizvodnje,vozac)
+values(null,'Suzuki','Swift','2015',2),
+values(null,'Opel','Corsa','2010','1');
+
+insert into voznja(putnik,vozilo)
+values(2,1),(1,1),(1,2);
