23836a56f94d17c82be8298aa209bac93f85120a
Kristijan Vidaković <kiki11223333@gmail.com>
2022-12-22 01:27:24 +0100

    Rjesen drugi zadatak

---
+++ eclipse/Edunova04/src/edunova/Z02.java
@@ -0,0 +1,18 @@
+package edunova;
+
+public class Z02 {
+	public static void main(String[] args) {
+		
+		// Program ispisuje brojeve od 100 do 1
+		// u istom redu odvojeno zarezom
+		
+		for(int i=100;i>0;i--) {
+			if(i==1) {
+				System.out.println(i);
+			}else {
+				System.out.print(i + ",");
+			}
+		}
+		
+	}
+}
