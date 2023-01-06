426cbe193f90dd9b9ee0c9fe305f62263cec7b46
Kristijan Vidaković <kiki11223333@gmail.com>
2023-01-06 21:07:45 +0100

    Neka pocetna verzija, ne valja jos

--- eclipse/Edunova04/src/edunova/CiklicnaMatrica.java
+++ eclipse/Edunova04/src/edunova/CiklicnaMatrica.java
@@ -4,8 +4,57 @@ public class CiklicnaMatrica {
 
 	public static void main(String[] args) {
 		// Kreirati program koji ispisuje cikličnu matricu
-		// Pocevsi od dolje desno   
-		
+		// Pocevsi od dolje desno
+
+		int broj = 1;
+
+		int k = 0;
+		int m = 3;
+		int l = 0;
+		int n = 3;
+		int matrica[][] = new int[m][n];
+
+		while (k < m && l < n) {
+			for (int i = l; i < n; ++i) {
+				matrica[k][i] = broj++;
+
+			}
+			k++;
+
+			for (int i = k; i < m; ++i) {
+				matrica[i][n - 1] = broj++;
+
+			}
+			n--;
+
+			if (k < m) {
+				for (int i = n - 1; i >= 1; --i) {
+					matrica[m - 1][i] = broj++;
+
+				}
+				m--;
+
+			}
+			if (l < n) {
+				for (int i = m - 1; i >= k; --i) {
+					matrica[i][l] = broj++;
+
+				}
+				l++;
+
+			}
+		}
+
+		m = 3;
+		n = 3;
+
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(matrica[i][j] + " ");
+			}
+			System.out.println();
+		}
+
 	}
 
 }
