73e85a3f8b762e2ccab4d23e22b660b3bcc95388
Kristijan Vidaković <kiki11223333@gmail.com>
2023-01-08 21:41:49 +0100

    Drugacije poravnanje tokom ispisa

--- eclipse/Edunova04/src/edunova/Tablica_Mnozenja25x.java
+++ eclipse/Edunova04/src/edunova/Tablica_Mnozenja25x.java
@@ -8,13 +8,13 @@ public class Tablica_Mnozenja25x {
 		for (int i = 1; i <= 25; i++) {
 			for (int j = 1; j <= 25; j++) {
 				if ((i * j < 10)) {
-					System.out.print(i * j + "   ");
+					System.out.print( "   " + i * j  );
 				}else if((i * j < 100)) {
-					System.out.print(i * j + "  ");
+					System.out.print("  " + i * j  );
 				}else if((i*j >10) && (i*j <100)) {
-					System.out.print(i * j + "   ");
+					System.out.print( "   " + i * j);
 				}else {
-					System.out.print(i * j + " ");
+					System.out.print(" " + i * j  );
 				}
 
 			}
