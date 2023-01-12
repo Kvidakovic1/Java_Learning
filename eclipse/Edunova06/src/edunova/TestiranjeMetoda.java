f1387747d6c889606b013f53d4c6d14d0fa57287
Kristijan Vidaković <kiki11223333@gmail.com>
2023-01-12 22:28:44 +0100

    Zadatak 5 zapeo

---
+++ eclipse/Edunova06/src/edunova/TestiranjeMetoda.java
@@ -0,0 +1,10 @@
+package edunova;
+
+public class TestiranjeMetoda {
+	public static void main(String[] args) {
+		double a = 3.14;
+		//double b = a / 10;
+		System.out.println(a-Math.floor(a));
+		
+	}
+}
---
+++ eclipse/Edunova06/src/edunova/Z05.java
@@ -0,0 +1,8 @@
+package edunova;
+
+public class Z05 {
+	// Metoda prima dva decimalna broja
+	// metoda vraća zbroj samo
+	// decimalnih vrijednosti
+
+}
