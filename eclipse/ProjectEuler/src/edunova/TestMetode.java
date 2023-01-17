aeeb4193401b7f9bff8e279388b7ededb2f02857
Kristijan Vidaković <kiki11223333@gmail.com>
2023-01-17 20:22:04 +0100

    Mali napredak sa kalkulatorom

---
+++ eclipse/ProjectEuler/src/edunova/TestMetode.java
@@ -0,0 +1,39 @@
+package edunova;
+
+public class TestMetode {
+
+	public static int []
+			ljubav(int[] niz) {
+
+		int[] noviNiz = new int[10];
+
+		if (niz.length % 2 == 0) {
+			noviNiz = new int[niz.length / 2];
+		} else {
+			noviNiz = new int[(niz.length / 2) + 1];
+		}
+
+		int duzina = noviNiz.length;
+
+		for (int i = 0; i < duzina; i++) {
+			if (i == duzina - 1) {
+				noviNiz[i] = niz[i];
+			} else {
+				noviNiz[i] = niz[i] + niz[niz.length - 1 - i];
+			}
+			
+		}
+		return noviNiz;
+
+	}
+
+	public static int zbroj(int broj) {
+		// uvjet prekida rekurzije
+		if (broj == 1) {
+			return 1;
+		}
+
+		return broj + zbroj(broj - 1) /* poziv rekurzije */;
+	}
+
+}
--- eclipse/ProjectEuler/src/edunova/test.java
+++ eclipse/ProjectEuler/src/edunova/test.java
@@ -2,12 +2,12 @@ package edunova;
 
 import java.util.Arrays;
 
-public class LjubavniKalkulator {
+public class test {
 
 	public static void main(String[] args) {
 		String ime1 = "Marta";
 		String ime2 = "Manuel";
-		String zajedno = ime1 + ime2;
+		String zajedno = ime1.toLowerCase() + ime2.toLowerCase();
 		int brojac = 0;
 		int [] brojevi = new int[zajedno.length()];
 		
@@ -27,33 +27,20 @@ public class LjubavniKalkulator {
 		System.out.println();
 		System.out.println(Arrays.toString(brojevi));
 		
+		System.out.println(TestMetode.ljubav(brojevi));
+		
+		
+		
+		
 		
 	}
 	
-	// Trenutna metoda public static int[] ljubav(int[] niz) {
-//	int[] noviNiz = new int[10];
-//	if (niz.length % 2 == 0) {
-//		noviNiz = new int[niz.length / 2];
-//	} else {
-//		noviNiz = new int[(niz.length / 2) + 1];
-//	}
-//
-//	int duzina = noviNiz.length;
-//
-//	for (int i = 0; i < duzina; i++) {
-//		if(i == duzina -1) {
-//			noviNiz[i] = niz[i];
-//		}else {
-//			noviNiz[i] = niz[i] + niz[niz.length - 1 - i];
-//		}
-//		
-//		
-//		
-//
-//	}
-//
-//	return noviNiz;
-//
-//}
+	
+	
+	
+	
+	
+		
+	
 
-}
+} 
