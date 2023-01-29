cb336b8ffbf510c32a24b1c14f8571bde62faf7a
Kristijan Vidaković <kiki11223333@gmail.com>
2023-01-29 22:11:27 +0100

    Rjesen Part 2

--- eclipse/AdventOfCode/src/advent/Day2_2022_Part2.java
+++ eclipse/AdventOfCode/src/advent/Day2_2022_Part2.java
@@ -3,5 +3,51 @@ package advent;
 import java.util.Scanner;
 
 public class Day2_2022_Part2 {
-	Scanner stdin = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner stdin = new Scanner(System.in);
+
+		int zbroj = 0;
+
+		while (true) {
+
+			String linija = stdin.next();
+			
+			if(linija.equals("kraj")) {
+				break;
+			}
+			if (linija.equals("AX")) {
+				zbroj += 3;
+			}
+			if (linija.equals("AY")) {
+				zbroj += 4;
+			}
+			if (linija.equals("AZ")) {
+				zbroj += 8; 
+			}
+			if (linija.equals("BX")) {
+				zbroj += 1;
+			}
+			if (linija.equals("BY")) {
+				zbroj += 5;
+			}
+			if (linija.equals("BZ")) {
+				zbroj += 9;
+			}
+			if (linija.equals("CX")) {
+				zbroj += 2;
+			}
+			if (linija.equals("CY")) {
+				zbroj += 6;
+			}
+			if (linija.equals("CZ")) {
+				zbroj += 7;
+			}
+			
+		}
+		stdin.close();
+		System.out.println(zbroj);
+
+	}
+	
+	
 }
