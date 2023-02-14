4564ef4d210509f769cd11f6b913399630bfcb20
Kristijan Vidaković <kiki11223333@gmail.com>
2023-02-14 21:42:41 +0100

    Test

---
+++ netbeans/HelloWorld/src/main/java/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package helloworld;
+
+/**
+ *
+ * @author Kristijan
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
